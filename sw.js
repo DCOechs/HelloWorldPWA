4cf68841b713d7563838906102bb9373deba6e1d
David Oechsler <david.c.oechsler@gmail.com>
2026-09-16 20:01:12 +0200

    Init hellopwa

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = "hello-v1";
+const FILES = ["./", "index.html"];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
